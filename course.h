1deddf73f1deea944df5e114a461a46d8e13578a
gowshik450526511 <66863804+gowshik450526511@users.noreply.github.com>
2023-10-03 15:09:02 +0530

    Update course.h

--- course.h
+++ course.h
@@ -4,6 +4,7 @@
 #include<iostream>
 #include<iomanip>
 #include<fstream>
+using namespace std;
 
 class course
 {
@@ -13,56 +14,53 @@ class course
 
     public:
 
-        void get_course_details(int);
-        int get_credit();
-        void print_course_details();
+        void course::get_course_details(int e)
+        {
+            if(e==1)
+            {
+                fstream r;
+                r.open("student.txt",ios::out | ios::app);
+        
+                cout<<"\n\nCOURSE NAME  :";
+                cin>>name;
+                cout<<"\n\nCOURSE CODE :";
+                cin>>code;
+                cout<<"\n\nCOURSE CREDIT :";
+                cin>>credit;
+        
+                r<<"\n\n"<<name<<"\t"<<code<<"\t"<<credit;
+                r.close();
+            }
+            else if(e==2)
+            {
+        
+                fstream r;
+                r.open("faculty.txt",ios::out | ios::app);
+        
+                cout<<"\n\nCOURSE NAME  :";
+                cin>>name;
+                cout<<"\n\nCOURSE CODE :";
+                cin>>code;
+                cout<<"\n\nCOURSE CREDIT :";
+                cin>>credit;
+        
+                r<<"\n\n"<<name<<"\t"<<code<<"\t"<<credit;
+                r.close();
+            }
+        }
+
+    int course::get_credit()
+    {
+        return credit;
+    }
+    
+    void course::print_course_details()
+    {
+        cout<<"\nNAME:"<<setw(15)<<name<<"\n\n";
+        cout<<"\nCODE:"<<setw(15)<<code<<"\n\n";
+        cout<<"\nCREDIT:"<<setw(15)<<credit<<"\n\n";
+    }
+
 };
-
-void course::get_course_details(int e)
-{
-    if(e==1)
-    {
-        fstream r;
-        r.open("student.txt",ios::out | ios::app);
-
-        cout<<"\n\nCOURSE NAME  :";
-        cin>>name;
-        cout<<"\n\nCOURSE CODE :";
-        cin>>code;
-        cout<<"\n\nCOURSE CREDIT :";
-        cin>>credit;
-
-        r<<"\n\n"<<name<<"\t"<<code<<"\t"<<credit;
-        r.close();
-    }
-    else if(e==2)
-    {
-
-        fstream r;
-        r.open("faculty.txt",ios::out | ios::app);
-
-        cout<<"\n\nCOURSE NAME  :";
-        cin>>name;
-        cout<<"\n\nCOURSE CODE :";
-        cin>>code;
-        cout<<"\n\nCOURSE CREDIT :";
-        cin>>credit;
-
-        r<<"\n\n"<<name<<"\t"<<code<<"\t"<<credit;
-        r.close();
-    }
-}
-
-int course::get_credit()
-{
-    return credit;
-}
-
-void course::print_course_details()
-{
-    cout<<"\nNAME:"<<setw(15)<<name<<"\n\n";
-    cout<<"\nCODE:"<<setw(15)<<code<<"\n\n";
-    cout<<"\nCREDIT:"<<setw(15)<<credit<<"\n\n";
-}
 
 
