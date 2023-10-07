54cf19badbaf6dbe0ef0bbcba3ed01b0345e5ed7
gowshik450526511 <66863804+gowshik450526511@users.noreply.github.com>
2023-10-07 15:26:31 +0530

    Update faculty_info.h

--- faculty_info.h
+++ faculty_info.h
@@ -3,54 +3,46 @@
 #include<fstream>
 #include "course.h"
 
-using namespace std;
-
-class faculty:public person
+class faculty : public person
 {
-    private:
-        string designation;
-        string dept;
-        int salary;
+private:
+    string designation;
+    string dept;
+    int salary;
 
-    public:
-        course *ptr;
-        void read();
-        void print_details();
+public:
+    std::unique_ptr<course> ptr; // Use std::unique_ptr for automatic memory management
 
+    void read()
+    {
+        cout << "\n\nGETTING FACULTY DETAILS:" << "\n\n";
+        get_details(2);
+        fstream v;
+        v.open("faculty.txt", ios::out | ios::app);
+        cout << "\n\nENTER THE DESIGNATION:";
+        cin >> designation;
+        cout << "\n\nENTER THE DEPARTMENT:";
+        cin >> dept;
+        cout << "\n\nENTER THE SALARY:";
+        cin >> salary;
+        v << "\n\n" << designation << "\t" << dept << "\t" << salary;
+
+        // Use std::make_unique to create and manage the memory
+        ptr = std::make_unique<course>();
+        ptr->get_course_details(2);
+
+        v.close();
+    }
+
+    void faculty::print_details() final
+    {
+        // Use std::format for advanced formatting
+        string output = std::format("\n\n####################################################################\n\n");
+        output += std::format("\n\nFACULTY NAME:{:<20}FACULTY AGE:{:<15}PHONE:{:<20}EMAIL:{:<25}STREET:{:<20}CITY:{:<20}STATE:{:<20}COUNTRY:{:<20}POSTAL CODE:{}\n",
+            name, age, phone_no, email_id, street_name, city, state, country, postal_code);
+        output += std::format("\n\nDESIGNATION:{:<35}DEPT:{:<25}SALARY:{}\n\n", designation, dept, salary);
+        output += std::format("########################################################################\n\n\n");
+    
+        cout << output;
+    }
 };
-
-void faculty::read()
-{
-    cout<<"\n\nGETTING FACULTY DETAILS:"<<"\n\n";
-    get_details(2);
-
-    fstream v;
-    v.open("faculty.txt",ios::out | ios::app);
-
-    cout<<"\n\nENTER THE DESIGNATION:";
-    cin>>designation;
-    cout<<"\n\nENTER THE DEPARTMENT:";
-    cin>>dept;
-    cout<<"\n\nENTER THE SALARY:";
-    cin>>salary;
-
-    v<<"\n\n"<<designation<<"\t"<<dept<<"\t"<<salary;
-
-    ptr=new course;
-    ptr->get_course_details(2);
-
-    v.close();
-}
-
-void faculty::print_details()
-{
-    cout<<"\n\n####################################################################\n\n";
-    cout<<"\n\n"<<"FACULTY NAME:"<<setw(20)<<"FACULTY AGE:"<<setw(15)<<"PHONE:"<<setw(20)<<"EMAIL:"<<setw(25)<<"STREET:"<<setw(20)<<"CITY:"<<setw(20)<<"STATE:"<<setw(20)<<"COUNTRY:"<<setw(20)<<"POSTAL CODE:"<<"\n";
-    cout<<name<<setw(17)<<age<<setw(25)<<phone_no<<setw(23)<<email_id<<setw(18)<<street_name<<setw(20)<<city<<setw(20)<<state<<setw(20)<<country<<setw(20)<<postal_code<<"\n\n";
-    cout<<"\n\n";
-    cout<<"DESIGNATION:"<<setw(35)<<"DEPT:"<<setw(25)<<"SALARY:"<<"\n\n";
-    cout<<designation<<setw(35)<<dept<<setw(25)<<salary<<"\n\n";
-    cout<<"########################################################################\n\n\n";
-}
-
-
