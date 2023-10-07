cfc106a0112505aaf2013fe7683f8197a1f385cd
gowshik450526511 <66863804+gowshik450526511@users.noreply.github.com>
2023-10-07 16:44:03 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,21 +8,23 @@ using namespace std;
 
 int main()
 {
-        int i,no_of_students,no_of_faculties;
+        int option;
+        int no_of_students;
+        int no_of_faculties;
         char ch;
 
         do
         {
         cout<<"1.Student\n2.Faculty\nENTER THE CHOICE:";
-        cin>>i;
+        cin>>option;
 
-        if(i==1)
+        if(option==1)
         {
             system("cls");
             cout<<"ENTER THE NO OF STUDENTS:";
             cin>>no_of_students;
 
-            student s[no_of_students];
+            vector<student> students(no_of_students);
 
             person *p;
 
@@ -30,36 +32,36 @@ int main()
 
             for(int i=0;i<no_of_students;i++)
             {
-                s[i].get_details(1);
-                s[i].student_details();
+                students[i].get_details(1);
+                students[i].student_details();
             }
-            for(int i=0;i<no_of_students;i++)
+            for(int j=0;j<no_of_students;j++)
             {
-                cout<<"\n\n"<<setw(40)<<"#################"<<"\t"<<"STUDENT DETAILS"<<"\t"<<"######################"<<"\n\n";
+                std::cout << std::format("\n\n{:<40}################# STUDENT DETAILS ######################\n\n", "");
 
-                s[i].print_details();
+                students[j].print_details();
             }
         }
-        else if(i==2)
+        else if(option==2)
         {
             system("cls");
             cout<<"ENTER THE NO OF FACULTIES:";
             cin>>no_of_faculties;
 
-            faculty f[no_of_faculties];
+            vector<faculty> faculties(no_of_faculties);
 
             person *p;
 
             p=&f[no_of_faculties];
 
-            for(int i=0;i<no_of_faculties;i++)//////////
-            {
-                f[i].read();
-            }
             for(int i=0;i<no_of_faculties;i++)
             {
-                cout<<"\n\n"<<setw(40)<<"#################"<<"\t"<<"FACULTY DETAILS"<<"\t"<<"######################"<<"\n\n";
-                f[i].print_details();
+                faculties[i].read();
+            }
+            for(int j=0;j<no_of_faculties;j++)
+            {
+                std::cout << std::format("\n\n{:<40}################# FACULTY DETAILS ######################\n\n", "");
+                faculties[j].print_details();
             }
         }
         cout<<"Do you want to continue :";
