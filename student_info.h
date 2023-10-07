5b338cd504bf3623a23d358aba32c724b62ec256
gowshik450526511 <66863804+gowshik450526511@users.noreply.github.com>
2023-10-07 15:09:40 +0530

    Update student_info.h

--- student_info.h
+++ student_info.h
@@ -1,63 +1,61 @@
-#include<iostream>
-#include<iomanip>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <format>
 #include "hostel_info.h"
 #include "student_course.h"
 
 using namespace std;
 
-class student:public person
+class student : public person
 {
-    private:
+private:
+    string reg_no;
+    string program;
 
-        char reg_no[100],program[100];
-//        fstream student_file;
+public:
+    hostel *h;
+    studentcourse *s;
 
-    public :
+    void student_details()
+    {
+        fstream p;
+        p.open("student.txt", ios::app | ios::out);
 
-        hostel *h;
+        cout << "\n\nENTER THE REGISTER NO:" << "\n";
+        cin >> reg_no;
 
-        studentcourse *s;
+        cout << "\n\nENTER THE PROGRAM:" << "\n";
+        cin >> program;
 
-        void student_details()
-        {
-            fstream p;
-            p.open("student.txt",ios::app | ios::out);
+        p << "\n\n" << reg_no << "\t" << program;
+        p.close();
 
-            cout<<"\n\nENTER THE REGISTER NO:"<<"\n";
-            cin>>reg_no;
+        cout << "\nENTER THE HOSTEL DETAILS:" << "\n\n";
+        h = new hostel;
 
-            cout<<"\n\nENTER THE PROGRAM:"<<"\n";
-            cin>>program;
+        h->get_hostel_details();
 
-            p<<"\n\n"<<reg_no<<"\t"<<program;
-            p.close();
+        cout << "\n\nCAL CGPA:" << "\n";
+        cal_cgpa();
+    }
 
-            cout<<"\nENTER THE HOSTEL DETAILS:"<<"\n\n";
-            h=new hostel;
+    void cal_cgpa()
+    {
+        s = new studentcourse;
+        s->get_courses();
+    }
 
-            h->get_hostel_details();
+    void print_details()
+    {
+        // Use std::format for advanced formatting
+        string output = std::format("\n\n\nNAME:{:<20}AGE:{:<20}PHONE:{:<20}EMAIL:{:<20}STREET:{:<20}CITY:{:<20}STATE:{:<20}COUNTRY:{:<20}POSTAL CODE:{}\n",
+            name, age, phone_no, email_id, street_name, city, state, country, postal_code);
 
-            cout<<"\n\nCAL CGPA:"<<"\n";
-            cal_cgpa();
-        }
-        void cal_cgpa()
-        {
-            s=new studentcourse;
-            s->get_courses();
-        }
-        void print_details()
-        {
-            cout<<"\n\n\nNAME:"<<setw(20)<<"AGE:"<<setw(20)<<"PHONE:"<<setw(20)<<"EMAIL:"<<setw(20)<<"STREET:"<<setw(20)<<"CITY:"<<setw(20)<<"STATE:"<<setw(20)<<"COUNTRY:"<<setw(20)<<"POSTAL CODE:"<<"\n";
-            cout<<name<<setw(17)<<age<<setw(20)<<phone_no<<setw(23)<<email_id<<setw(18)<<street_name<<setw(20)<<city<<setw(20)<<state<<setw(20)<<country<<setw(20)<<postal_code<<"\n\n";
+        output += std::format("\n\nREG:{:<20}PROG:{}\n", reg_no, program);
 
-            cout<<"\n\n"<<"REG:"<<setw(20)<<"PROG:"<<"\n\n";
-            cout<<reg_no<<setw(20)<<program<<"\n\n";
+        cout << output;
 
-            h->print_hostel_details();
-
-            s->print_student_course_details();
-        }
+        h->print_hostel_details();
+        s->print_student_course_details();
+    }
 };
-
-
